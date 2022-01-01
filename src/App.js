e1c4ea46cb6b43629aa34b416e3fbebc9f1c5857
nykachi <nykachi@gmail.com>
2022-01-01 18:16:23 -0500

    0101

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Skills from "./components/Skills/Skills";
+import YouTube from "./components/YouTube/YouTube";
+import Contact from "./components/Contact/Contact";
+import ReactPlayer from "react-player";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     < NavBar />
+     <ReactPlayer controls url='https://www.youtube.com/watch?v=MD2Q6cqVmL4&ab_channel=JayLim' />
+     < Home />
+     < About />
+     < Skills />
+     < YouTube />
+     < Contact />
     </div>
   );
 }
