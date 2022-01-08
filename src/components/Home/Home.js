8d4fb577457a39a2766a018d4aa0199b3cc0f563
nykachi <nykachi@gmail.com>
2022-01-07 19:25:37 -0500

    0107-01

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,44 @@
-
+import classes from './Home.module.css'
+import lightjoo from './../../img/lightjoo.jpg'
 
 
 const Home = () => {
     return (
+    
+    <div className={classes.home}>
+        
+      <section className={classes.info}>
+        <ul>  
+            <li className={classes.item}>
+                <div className={classes.img}>
+                    <img className={classes.lightjoo} src={lightjoo} alt=""></img>
+                    대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 </div>
+                <div className={classes.img}>
+                    <img className={classes.lightjoo} src={lightjoo} alt=""></img>
+                    대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 </div>
+            </li>    
+            
+            <li className={classes.item}>
+                <div className={classes.img}>
+                    <img className={classes.lightjoo} src={lightjoo} alt=""></img>
+                    대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 </div>
+                <div className={classes.img}>
+                    <img className={classes.lightjoo} src={lightjoo} alt=""></img>
+                    대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 </div>
+            </li> 
 
-        <div className='Home'>
-            <h1>Home</h1>
-        </div> 
+            <li className={classes.item}>
+                <div className={classes.img}>
+                    <img className={classes.lightjoo} src={lightjoo} alt=""></img>
+                    대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 </div>
+                <div className={classes.img}>
+                    <img className={classes.lightjoo} src={lightjoo} alt=""></img>
+                    대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 대한민국 </div>
+            </li> 
+                
+        </ul>
+      </section>
+    </div> 
      );
 }
  
